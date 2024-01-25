550bab259ba3a1187726d7ca389959ccb709b3ec
josephmangara <josephmangara2@gmail.com>
2024-01-25 11:10:28 +0300

    done

--- lib/get.py
+++ lib/get.py
@@ -1,10 +1,17 @@
 import requests
 import json
 
-url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
+url = "https://rickandmortyapi.com/api/character//1,2,3,4,5,6,7,8,9,10,11,12,13"
 
 response = requests.get(url)
 
 json_content = json.loads(response.text)
 
 print(json.dumps(json_content, indent=4))
+print(response.text)
+
+
+#simple fetch request
+# url = "https://rick-and-morty-lyart-psi.vercel.app/"
+# response = requests.get(url)
+# print(response.text)
